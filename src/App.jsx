f419cc5f09987eebe5e177103e0b379989ddf7f5
Pablo Escales <pabloescales@gmail.com>
2023-04-08 22:34:00 +0200

    share button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import Card from './Card';
+import { Card } from './Card';
 import QR_CODE from './images/qrcode_pablofolio.png'
 
 
-function App({ qr }) {
+function App() {
   return (
     <div className='app'>
       <Card qr={QR_CODE}/>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 
-const Card = ({ qr }) => {
+export function Card ({ qr }) {
 
   const [fullName, setFullName] = useState('Pablo Escales');
   const [role, setRole] = useState('Frontend Web Developer');
@@ -20,32 +20,26 @@ const Card = ({ qr }) => {
   return (
     <>
       <main className='main'>
-          <form className='form' >
-            <h1>Create your QR code</h1>
-            <input
-              onChange={handleFullNameChange}
-              // value={search}
-              name='query'
-              placeholder="Full name..."
-            />
-            <input
-              onChange={handleRoleChange}
-              // value={search}
-              name='query'
-              placeholder="Role..."
-            />
-            <button type='submit'>Save</button>
+        <form className='form'>
+          <h1>Create your QR code</h1>
+          <input style={{padding: '5px', border: 'solid 3px' , borderColor: '', borderRadius: '8px', marginRight: '6px'}}
+            onChange={handleFullNameChange}
+            placeholder="Full name..."
+          />
+          <input style={{padding: '5px', border: 'solid 3px' , borderColor: '', borderRadius: '8px', marginRight: '6px'}}
+            onChange={handleRoleChange}
+            placeholder="Role..."
+          />
+          <button style={{padding: '5px', border: 'solid 3px' , borderColor: 'orange', borderRadius: '8px'}} type='submit'>Share</button>
         </form>
       </main>
 
-        <div className='container'>
-            <h2>{fullName}</h2>
-            <h3>{role}</h3>
-            <img src={qr} alt="QR_Code" width='100%' />
-            <p>Scan the QR code to visit my website</p>
-        </div>
+      <card className='container'>
+        <h2>{fullName}</h2>
+        <h3>{role}</h3>
+        <img src={qr} alt="QR_Code" width='100%' />
+        <p>Scan the QR code to visit my website</p>
+      </card>
     </>
   )
 }
-
-export default Card
